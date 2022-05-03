a42ad2b80cd9e12e36788da7ef41d0593066f0c3
Arthur <arthur_2335@hotmail.com>
2022-05-03 15:26:24 -0400

    connected to DB

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes GO HERE... -- Defining which requests are allowed -- 
+
+app.listen(3000, () => {
+    console.log('Listening on http://localhost:3000...')
+});
+
---
+++ src/connectDb.js
@@ -0,0 +1,21 @@
+// Import tools from firebase
+import { firestore } from 'firebase-admin';
+import { initializeApp, getApps, cert  } from 'firebase-admin/app';
+// Import firestore from firebase
+import {getFirestore} from 'firebase-admin/firestore';
+// Import my credentials to connect to firebase
+import myCredentials from '../credentials.js';
+
+// Write a function to connect to firebase
+export default function connectDb() {
+    // First check to see if we are already connected...
+    if(getApps().length === 0){
+            // If not, connect..
+            initializeApp({
+                credential: cert(myCredentials)
+            });
+    }
+    // Either way...
+    return getFirestore();
+    // Return the connection to firestore
+}
