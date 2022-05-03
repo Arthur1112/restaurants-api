f9fd8fd90c50e30695dfe6cccabb4f1958c35ca8
Arthur <arthur_2335@hotmail.com>
2022-05-03 16:27:41 -0400

    added addRestaurant and getAllRestaurant

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
-
+import { addRestaurant, getAllRestaurant } from './src/restaurants.js';
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // Routes GO HERE... -- Defining which requests are allowed -- 
+app.post('/restaurants', addRestaurant);
+app.get('/restaurants', getAllRestaurant);
 
 app.listen(3000, () => {
     console.log('Listening on http://localhost:3000...')
---
+++ src/restaurants.js
@@ -0,0 +1,46 @@
+import connectDb from "./connectDb.js";
+
+export const addRestaurant = async (req, res) => {
+    // Check if request is valid
+    if(!req.body || !req.body.name || !req.body.address) {
+        res.status(401).send('Invalid request');
+        return;
+    }
+    // connect to Firestore
+    const db = connectDb();
+    // Prepare the data
+    const newRestaurant = {
+        name: req.body.name,
+        address: req.body.address,
+        rating: req.body.rating || 3,
+        cuisine: req.body.cuisine || 'American',
+    }
+    // Add data to the restaurant collection
+    try{
+        const doc = await db.collection('restaurants').add(newRestaurant);
+        // respond with success
+        res.status(201).send('Restaurant created ' + doc.id);
+    } catch (err){
+    // respond with error
+    res.status(500).send(err);
+}
+// db.collection('restaurant').add(newRestaurant)
+// .then(doc => res.status(201).send('Restaurant created ' + doc.id))
+// .catch(err => res.status(500).send(err))
+}
+
+
+export const getAllRestaurant = async (req, res) => {
+    const db = connectDb();
+    try{
+        const snapshot = await db.collection('restaurants').get();
+        const restaurantsArray = snapshot.docs.map(doc => {
+            let restaurant = doc.data();
+            restaurant.id = doc.id;
+            return restaurant;
+        })
+        res.send(restaurantsArray);
+    } catch (err){
+        res.status(500).send(err);
+    }
+}
